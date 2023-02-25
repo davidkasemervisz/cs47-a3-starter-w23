4f1588e344138e04762d631944b7588df5ea7fbc
davidkasemervisz <davidkaz@stanford.edu>
2023-02-24 23:49:42 -0800

    a4

--- App.js
+++ App.js
@@ -1,52 +1,35 @@
+import 'react-native-gesture-handler';
+import React, {Component} from 'react';
 import { FlatList, Pressable, View, Image, StyleSheet, SafeAreaView, Text } from "react-native";
 import { useSpotifyAuth } from "./utils";
 import { Themes } from "./assets/Themes";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { WebView } from 'react-native-webview';
 
-export default function App() {
-  // Pass in true to useSpotifyAuth to use the album ID (in env.js) instead of top tracks
-  const { token, tracks, getSpotifyAuth } = useSpotifyAuth(true);
+const Stack = createStackNavigator();
 
-  console.log("tracks", tracks);
+const renderSong = ({item}, props) => {
+  const navigation = props;
+  const details_site = item.externalUrl;
+  const preview_site = item.previewUrl;
 
-  if(token == "") { // authenticate button 
-    return (
-      <SafeAreaView style = {styles.container}>
-        <Pressable style={styles.authenticate_button}
-          onPress={getSpotifyAuth}>
-          <Image 
-            style={{width: "20%", height: "100%", resizeMode: "contain"}} 
-            source={require('./assets/Images/spotify-logo.png')} 
-          />
-          <Text style={{color: "white", fontSize: 24}}>Connect With Spotify</Text>
-        </Pressable>
-      </SafeAreaView>
-    );
-  } 
-  
-  else { // display songs
-    const song_index = (title) => {
-      for(var i = 0; i < tracks.length; i++) {
-        if(tracks[i].songTitle == title) {
-          return i+1;
-        }
-      }
-
-      return -1;
-    }
-
-    const millisToMinutesAndSeconds = (millis) => {
+  const millisToMinutesAndSeconds = (millis) => {
       const minutes = Math.floor(millis / 60000);
       const seconds = ((millis % 60000) / 1000).toFixed(0);
       return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
     };
 
-    const renderSong = ({item}) => {
-      console.log('items', item)
       return (
+        <Pressable onPress={() => navigation.navigate("SongDetailsScreen", {site: details_site})}>
         <View style={styles.song_container}>
-          <Text style={{color: "white", padding: 4}}>
-            {song_index(item.songTitle)}
-          </Text>
+          <Pressable style={{width: "7.5%", height: "25%"}}
+          onPress={() => {navigation.navigate("SongPreviewScreen", {site: preview_site})}}> 
+            <Image 
+              style = {{width: "100%", height: "100%", resizeMode: "contain"}}
+              source = {require('./assets/Images/Spotify-Play-Button.png')}
+            />
+          </Pressable>
           <Image 
             style={{width: "20%", height: "100%", resizeMode: "contain"}} 
             source={{uri: item.imageUrl}} 
@@ -70,11 +53,17 @@ export default function App() {
             </Text>
           </View>
         </View>
+        </Pressable>
       );
     };
 
-    return (
-      <SafeAreaView style = {styles.container}>
+export default function App() {
+  // Pass in true to useSpotifyAuth to use the album ID (in env.js) instead of top tracks
+  const { token, tracks, getSpotifyAuth } = useSpotifyAuth(true);
+
+const AlbumScreen = ({navigation}) => {
+  return (
+    <SafeAreaView style={styles.container}>
         <View style= {styles.header}>
           <Image 
             style={{width: "20%", height: "100%", resizeMode: "contain"}} 
@@ -84,10 +73,76 @@ export default function App() {
         </View>
         <FlatList
           data = {tracks}
-          renderItem = {(item) => renderSong(item)}
+          renderItem = {(item) => renderSong(item, navigation)}
           keyExtractor = {(item) => item.songTitle}
         />
       </SafeAreaView>
+  );
+}
+
+const SongDetailsScreen = ({navigation, route}) => {
+  const site = route.params.site;
+  console.log("site", site);
+
+  return (
+    <View style={{width: "100%", height: "100%"}}>
+      <WebView
+          source={{ uri: site }}
+          style={{ width: '100%', height: '100%' }}
+        />
+    </View>
+  );
+};
+
+const SongPreviewScreen = ({navigation, route}) => {
+  const site = route.params.site;
+  console.log("site", site);
+
+  if(site == undefined) {
+    return (
+      <View style={{alignItems: "center", justifyContent: "center"}}>
+        <Text>No Preview Exists</Text>
+      </View>
+    )
+  }
+
+  else {
+    return (
+      <View style={{width: "100%", height: "100%"}}>
+      <WebView
+          source={{ uri: site }}
+          style={{ width: '100%', height: '100%' }}
+        />
+    </View>
+    );
+  }
+};
+
+  if(token == "") { // authenticate button 
+    return (
+      <SafeAreaView style = {styles.container}>
+        <Pressable style={styles.authenticate_button}
+          onPress={getSpotifyAuth}>
+          <Image 
+            style={{width: "20%", height: "100%", resizeMode: "contain"}} 
+            source={require('./assets/Images/spotify-logo.png')} 
+          />
+          <Text style={{color: "white", fontSize: 24}}>Connect With Spotify</Text>
+        </Pressable>
+      </SafeAreaView>
+    );
+  } 
+
+  else { // display songs
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="AlbumScreen" component={AlbumScreen} 
+              options={{headerShown: false}} />
+          <Stack.Screen name="SongDetailsScreen" component={SongDetailsScreen} />
+          <Stack.Screen name="SongPreviewScreen" component={SongPreviewScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   }
 }
